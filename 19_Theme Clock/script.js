f95c881ef1795e65c64feceec67759173d6f0ff8
kimbomi <kimbomi2172@naver.com>
2023-03-06 16:10:26 +0900

    19 commit

---
+++ 19_Theme Clock/script.js
@@ -0,0 +1,51 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const  toggle = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html');
+  if(html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    e.target.innerHTML = 'Dark mode';
+  } else {
+    html.classList.add('dark');
+    e.target.innerHTML = 'Light mode';
+  }
+})
+
+function setTime() {
+  // 현재 날짜와 시간을 보여줌
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours >= 13 ? hours % 12 : hours;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  // 시계는 360도 회전하기 때문에 0~360까지 매핑(시는 12, 분은 60, 초는 60)
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+
+}
+ // https:stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+ const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+// 매초마다 setTime 간격을 줌.
+setInterval(setTime, 1000);
